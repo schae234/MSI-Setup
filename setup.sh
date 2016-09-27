49561aac157cb16fcb651004111bb9b2e05c2c32
Rob <schae234@umn.edu>
2016-09-27 10:31:00 -0500

    add fix permissions function to profile

--- setup.sh
+++ setup.sh
@@ -13,6 +13,13 @@ cat << EOF > $HOME/.mccuem_profile
 export PATH=$CONDA_BASE/bin:\$PATH
 export PATH=$SHARED_BASE/bin:\$PATH
 umask 0027
+
+function fix-permissions {
+    find /home/mccuem/ -user $(whoami) -type f -exec chmod g+r {} \;
+    find /home/mccuem/ -user $(whoami) -type d -exec chmod g+rx {} \;
+}
+
+
 EOF
 
 if [[ $(grep "source \$HOME/.mccuem_profile" $HOME/.bashrc | wc -l ) != 1 ]]; then
