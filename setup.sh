28e10ff1defe620a245c84e6fcd6eae4c7271d3a
Rob Schaefer <schae234@umn.edu>
2016-09-27 14:36:10 -0500

    add echos

--- setup.sh
+++ setup.sh
@@ -10,13 +10,19 @@ SHARED_BASE='/home/mccuem/shared/.local'
 
 cat << EOF > $HOME/.mccuem_profile
 # McCuelab Shared directives
+# Installed $(date)
 export PATH=$CONDA_BASE/bin:\$PATH
 export PATH=$SHARED_BASE/bin:\$PATH
+export LD_LIBRARY_PATH=/home/mccuem/schaefe2/.local/lib:\$LD_LIBRARY_PATH
+
 umask 0027
 
 function fix-permissions {
-    find /home/mccuem/ -user $(whoami) -type f -exec chmod g+r {} \;
-    find /home/mccuem/ -user $(whoami) -type d -exec chmod g+rx {} \;
+    echo "Fixing permissions...."
+    find /home/mccuem/ ! -readable -prune -o -user $(whoami) -type f -exec chmod g+r {} \; -o -user $(whoami) -type d -exec chmod g+rx {} \;
+    echo ".ssh folder cannot be group readable"
+    chmod -R g-rw ~/.ssh
+    echo "DONE!"
 }
 
 
@@ -32,4 +38,3 @@ cat <<EOF
 INSTALLATION COMPLETE
 EOF
 
-return 0
