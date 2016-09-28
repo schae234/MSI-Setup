d8b28c7c68aa8f41717b05df7ba35d8f1ef45207
Rob Schaefer <schae234@umn.edu>
2016-09-28 12:59:18 -0500

    Now use a shared profile

--- setup.sh
+++ setup.sh
@@ -6,35 +6,15 @@
 CONDA_BASE="/home/mccuem/shared/.local/conda"
 SHARED_BASE='/home/mccuem/shared/.local'
 
+PWD="`dirname \"$BASH_SOURCE\"`"              # relative
+PWD="`( cd \"$PWD\" && pwd )`"  # absolutized and normalized
 
+echo "Currently in $PWD"
 
-cat << EOF > $HOME/.mccuem_profile
-# McCuelab Shared directives
-# Installed $(date)
-export PATH=$CONDA_BASE/bin:\$PATH
-export PATH=$SHARED_BASE/bin:\$PATH
-export LD_LIBRARY_PATH=/home/mccuem/shared/.local/lib:\$LD_LIBRARY_PATH
-
-umask 0027
-
-function fix-permissions {
-    echo "Fixing permissions...."
-    find /home/mccuem/ ! -readable -prune -o -user $(whoami) -type f -exec chmod g+r {} \; -o -user $(whoami) -type d -exec chmod g+rx {} \;
-    echo ".ssh folder cannot be group readable"
-    chmod -R g-rw ~/.ssh
-    echo "DONE!"
-}
-
-
-EOF
-
-if [[ $(grep "source \$HOME/.mccuem_profile" $HOME/.bashrc | wc -l ) != 1 ]]; then
-    echo "source \$HOME/.mccuem_profile" >> $HOME/.bashrc
+if [[ $(grep "mccuem_profile" $HOME/.bashrc | wc -l ) != 1 ]]; then
+    echo "source $PWD/mccuem_profile" >> $HOME/.bashrc
 fi
 
-source $HOME/.mccuem_profile
+source $PWD/mccuem_profile
 
-cat <<EOF
-INSTALLATION COMPLETE
-EOF
-
+echo INSTALLATION COMPLETE
